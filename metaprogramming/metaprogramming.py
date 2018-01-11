740b5a59841b1c89fc7a3bba3e30d209bbc5b482
weikailu <weikailu96@gmail.com>
2018-01-11 13:41:28 -0700

    ini & metaprg

---
+++ metaprogramming/metaprogramming.py
@@ -0,0 +1,25 @@
+'''
+Metaprogramming
+
+problem site: https://open.kattis.com/problems/metaprogramming
+
+Difficulty: 2.8
+CPU Time limit: 4 seconds
+Memory limit: 1024 MB
+'''
+
+def parseInput(line):
+	pass
+
+def update_label(label, value):
+	pass
+
+def evaluate(label_1, label_2, operand):
+	pass
+
+def main():
+	label = {}
+
+
+if __name__ == '__main__':
+	main()
